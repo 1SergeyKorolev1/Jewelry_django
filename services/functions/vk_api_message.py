fe54d3900c3fdc2fbc3a829c562129520b19be63
Sergey Korolev <bongrichard@rambler.ru>
2024-11-17 22:43:54 +0300

    настроена функция отправки изображений

--- services/functions/vk_api_message.py
+++ services/functions/vk_api_message.py
@@ -5,14 +5,24 @@ from vk_api.utils import get_random_id
 from config.settings import TOKEN_VK
 from services.functions.number_order import get_number_order_list
 from services.models import Making
+from vk_api.upload import VkUpload
+import pathlib
+project_path = pathlib.Path(__file__).parent.parent.parent
 
 vk_session = vk_api.VkApi(token=TOKEN_VK)
 session_api = vk_session.get_api()
 longpool = VkLongPoll(vk_session)
+upload = VkUpload(vk_session)
 
 def send_some_message(user_id, some_text):
     session_api.messages.send(user_id=user_id, message=some_text, random_id=get_random_id())
     # vk_session.method('messages.send', {'user_id': id, 'message': some_text, 'random_id': get_random_id()})
+
+def send_some_image(peer_id, image, user_id):
+    with open(pathlib.Path(project_path, f'media/{image}'), 'rb') as img_:
+        upload_img = upload.photo_messages(photos=[img_, ], peer_id=peer_id)[0]
+        pic = 'photo{}_{}'.format(upload_img['owner_id'], upload_img['id'])
+        session_api.messages.send(user_id=user_id, random_id=get_random_id(), attachment=pic, message='Сообщение')
 
 def get_help_message(info_):
     first_name = info_[0]['first_name']
@@ -24,8 +34,12 @@ def get_help_message(info_):
 
 def get_order_message(number):
     making = Making.objects.filter(number=number)
-    print(making)
-    return 'gjrf gjujlm'
+    return (f'Заказ №{number} {making[0]} передан на рассмотрение мастеру.\n'
+            f'Ожидайте ответа(ответ придет в этой беседе)')
+
+def get_order_image(number):
+    making = Making.objects.filter(number=number)
+    return making[0].image_one
 
 @shared_task
 def start_vk_bot():
@@ -36,9 +50,11 @@ def start_vk_bot():
                 msg = event.text.lower()
                 id_ = event.user_id
                 info = session_api.users.get(user_id=id_)
+                peer_id = event.peer_id
                 if msg == 'help':
                     send_some_message(id_, get_help_message(info))
                 if msg == 'мой id':
                     send_some_message(id_, f'Ваш vk id: {id_}')
                 if msg in get_number_order_list():
                     send_some_message(id_, get_order_message(msg))
+                    send_some_image(peer_id, get_order_image(msg), id_)
